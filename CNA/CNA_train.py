86b065aef9256e51316845aa3a2f1c396622b929
Aleyna Er <41549412+aleynaer@users.noreply.github.com>
2021-06-10 01:29:34 +0300

    Add files via upload

--- CNA/CNA_train.py
+++ CNA/CNA_train.py
@@ -39,3 +39,115 @@ y_cols = y_cols.apply(le.fit_transform)
 final_df = pd.concat([x_cols, y_cols], axis = 1)
 
 #%%  *** your ml code here ***
+#%% KNN algorithm
+
+from sklearn import neighbors
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+X_train, X_test, y_train, y_test =train_test_split(x_cols, y_cols, test_size=.25, random_state=17)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+print(clf)
+
+# knn evaluate
+y_pred= clf.predict(X_test)
+y_expect = y_test
+
+print(metrics.classification_report(y_expect, y_pred))
+
+# knn save model
+
+import joblib
+  
+# Save the model as a pickle in a file
+joblib.dump(clf, 'CNA-knn-primHist.pkl')
+
+#%% #%% random forest algorithm
+
+from sklearn.model_selection import train_test_split 
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+
+X_train, X_test, y_train, y_test =train_test_split(x_cols, y_cols, test_size=.25, random_state=17)
+
+classifier = RandomForestClassifier(n_estimators=200, random_state=0)
+
+
+
+classifier.fit(X_train, y_train)
+
+# random forest evaluate
+yy_pred = classifier.predict(X_test)
+print(metrics.classification_report(y_test, yy_pred))
+
+#  random forest save model
+import joblib
+  
+# Save the model as a pickle in a file
+joblib.dump(classifier, 'CNA-rforest-primHist.pkl')
+
+#%% naive bayes algorithm
+
+from sklearn.naive_bayes import CategoricalNB
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+
+X_train, X_test, y_train, y_test = train_test_split(x_cols, y_cols, test_size=.25, random_state=17)
+
+CatNB = CategoricalNB()
+CatNB.fit(X_train, y_train)
+
+y_expect = y_test
+y_pred = CatNB.predict(X_test)
+
+print(accuracy_score(y_expect, y_pred))
+
+print(metrics.classification_report(y_expect, y_pred))
+
+
+# naive bayes save model
+
+import joblib
+  
+# Save the model as a pickle in a file
+joblib.dump(CatNB, 'CNA-naiveBayes-primHist.pkl')
+
+
+#%% logistic regression algorithm
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+X_train, X_test, y_train, y_test = train_test_split(x_cols, y_cols, test_size=.25, random_state=17)
+
+LogReg = LogisticRegression(solver='liblinear',multi_class='ovr', random_state=200)
+LogReg.fit(X_train, y_train)
+
+# Log Reg evaluate 
+from sklearn.metrics import classification_report
+
+y_pred = LogReg.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+print('Training accuracy:', LogReg.score(X_train, y_train))
+print('Test accuracy:', LogReg.score(X_test, y_test))
+
+# Log reg save model
+import joblib
+  
+# Save the model as a pickle in a file
+joblib.dump(LogReg, 'CNA-LogReg-primHist.pkl')
+
+#%% primary histology tahmininde accuracy'ler; 
+
+# KNN = %
+# Random forest = %
+# naive bayes = %
+# logistic reg = %
+
+#%% cheomosome için ??
+
